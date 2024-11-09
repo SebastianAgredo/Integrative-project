fc07dc76952ab484fb84175d040474d33397172e
Sebastian Agredo <sebastian.agredo@uniminuto.edu.co>
2024-11-09 02:21:58 +0000

    EVIDENCIA SEMANA 14

--- bundles/models/proffesor.py
+++ bundles/models/proffesor.py
@@ -1,8 +1,8 @@
 from models.person import Person
 
 class Proffesor(Person):
-    def __init__(name,lastname,age,bachelor=True):
-        super().__init__(name,lasname,age)
+    def __init__(self,name,lastname,age,bachelor=True):
+        super().__init__(name,lastname,age)
 
     def __str__(self):
         return f"Proffesor(name={self.name},lastname= {self.lastname})"
--- bundles/models/student.py
+++ bundles/models/student.py
@@ -1,8 +1,8 @@
 from models.person import Person
 
 class Student(Person):
-    def __init__(self, name, lastname, age):
-        super__init__(name,lastname,age)
+    def __init__(self, name, lastname, age,code):
+        super().__init__(name,lastname,age)
         self.code=code
 
 
@@ -10,6 +10,5 @@ class Student(Person):
         return f"Student(name){self.name},lastname={self.lastname},age={self.age},code={self.code}"
 
 
-    @Abstractmethod
     def sayHello(self):
         return f"Hola soy estudiante y me llamo {self.name}"
